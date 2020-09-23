8528220e068177925586c56d1e44301ea7dcfa74
qijiao <1003297709@qq.com>
2020-09-23 11:15:17 +0800

    对话框组件-用作单独更新样式

---
+++ src/components/elementUi/dialog.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="hj-dialog">
+    <el-dialog
+      v-bind="$attrs"
+      v-on="$listeners"
+      :title="title"
+      :close-on-click-modal="closeOnClickModal"
+    >
+      <slot></slot>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+import { Dialog } from 'element-ui'
+export default {
+  name: 'hjDialog',
+  components: {
+    elDialog: Dialog
+  },
+  props: {
+    title: { type: String },
+    closeOnClickModal: {
+      type: Boolean,
+      default: false
+    },
+    dialogDrag: {
+      type: Boolean,
+      default: true
+    }
+  },
+  data () {
+    return {}
+  },
+  methods: {},
+  mounted () {}
+}
+</script>
+
+<style>
+
+</style>
