4d2e4243988f6d6d03c79a3d295458630e3ad620
qijiao <1003297709@qq.com>
2020-09-23 11:16:22 +0800

    答题卡布局-div

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -5,7 +5,17 @@
       <span class="score_style">0</span>
       <span>分</span>
     </div>
-    <div class="layout-setting"></div>
+    <div class="layout-setting">
+      <div class="layout-top">
+        <span>布局</span>
+        <span class="layout-edit">修改</span>
+      </div>
+      <div class="layout-bottom">
+        <div>A3/B4/8K纸</div>
+        <div>两栏</div>
+        <!-- <div>线上阅卷</div> -->
+      </div>
+    </div>
   </div>
 </template>
 
@@ -16,5 +26,40 @@ export default {
 </script>
 
 <style lang="less" scoped>
-
+  @import '~@/assets/css/publicColor.less';
+  .layout-content{
+    position: fixed;
+    top: 90px;
+    left: 10px;
+  }
+  .total-score{
+    font-size: 14px;
+    .score_style{
+      font-size: 18px;
+      font-weight: 500;
+      margin: 0 5px;
+      color: @main
+    }
+  }
+  .layout-setting{
+    border: 1px solid @main;
+    min-width: 93px;
+    background: @white;
+    margin-top: 10px;
+    font-size: 14px;
+    .layout-top{
+      background-color: @font-f7f8;
+      padding: 5px 8px;
+    }
+    .layout-edit {
+      color: @mainFont !important;
+      cursor: pointer;
+      float: right;
+    }
+    .layout-bottom {
+      background-color: @white;
+      padding: 10px 8px;
+      line-height: 22px;
+    }
+  }
 </style>
