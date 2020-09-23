320646bcf9cb44430b5fb6e9a4b0b3cd4da326c0
qijiao <1003297709@qq.com>
2020-09-23 16:22:52 +0800

    更改对话框样式

--- src/components/elementUi/dialog.vue
+++ src/components/elementUi/dialog.vue
@@ -37,6 +37,54 @@ export default {
 }
 </script>
 
-<style>
+<style lang="less">
+  @import '~@/assets/css/publicColor.less';
+  .el-dialog__header {
+    text-align: center;
+    padding: 10px 0;
+    background-color: #f8f8f8;
+  }
 
+  .el-dialog__headerbtn {
+    top: 10px;
+    right: 12px;
+  }
+  .el-dialog__title {
+    color: #333;
+  }
+  .el-dialog__headerbtn .el-dialog__close {
+    /* color: white; */
+    font-size: 23px;
+  }
+  .public-dialog .el-button {
+    padding: 8px 15px;
+  }
+  .el-dialog__body {
+    text-align: left;
+    padding: 10px 20px 15px;
+  }
+
+  .dialog-footer {
+    text-align: right;
+    padding-top: 20px;
+  }
+  .dialog-footer > div {
+    display: inline-block;
+  }
+  .dialog-footer > div:nth-of-type(1) {
+    margin-right: 15px;
+  }
+  .el-button{
+    font-size: 14px;
+    border: none;
+  }
+  .el-button.cancel{
+    margin-right: 15px;
+    background-color: #f1f1f1;
+    color:@font-666
+  }
+  .el-button.confirm{
+    background-color: @main;
+    color: @white
+  }
 </style>
