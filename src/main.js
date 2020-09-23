13450e3c138e1bfca08d6bf2ee1bd2a7d90521a7
qijiao <1003297709@qq.com>
2020-09-23 14:52:29 +0800

    注册element-ui Row组件

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import { Dialog, Select, Button, Checkbox } from 'element-ui'
+import { Dialog, Select, Button, Checkbox, Row } from 'element-ui'
 
 import App from './App.vue'
 import router from './router'
@@ -9,6 +9,7 @@ Vue.use(Dialog)
 Vue.use(Select)
 Vue.use(Button)
 Vue.use(Checkbox)
+Vue.use(Row)
 Vue.config.productionTip = false
 
 new Vue({
