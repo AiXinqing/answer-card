ad0cbc58af9a4aa315f4a977b597d62fb9b00a80
qijiao <1003297709@qq.com>
2020-09-23 10:06:17 +0800

    答题卡布局页面

---
+++ src/views/Layout.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="layout-content">
+    <div class="total-score">
+      <span>当前总分:</span>
+      <span class="score_style">0</span>
+      <span>分</span>
+    </div>
+    <div class="layout-setting"></div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
