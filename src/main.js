2af29422589e32482596eb5bfd970cad2735dcdb
qijiao <1003297709@qq.com>
2020-09-23 11:41:59 +0800

    注册 element-ui select button组件

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
-import { Dialog } from 'element-ui'
+import { Dialog, Select, Button } from 'element-ui'
 
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
 Vue.use(Dialog)
+Vue.use(Select)
+Vue.use(Button)
 Vue.config.productionTip = false
 
 new Vue({
