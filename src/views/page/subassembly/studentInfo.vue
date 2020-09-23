4bbc5d858db5c692273ae295299f13cfb295f528
qijiao <1003297709@qq.com>
2020-09-23 18:32:08 +0800

    答题卡标题子组件

---
+++ src/views/page/subassembly/studentInfo.vue
@@ -0,0 +1,65 @@
+<template>
+  <el-row class="TestTitle">
+    <el-col
+      v-for="(item, i) in studentInfoArr"
+      :key="i"
+      :span="4"
+      class="title-item"
+    >
+      <span>{{ item.name }}</span>
+      <span v-if="i == studentInfoArr.length - 1">
+        <span
+          v-show="!previewIs"
+          class="titke-edit"
+          @click="editStudentInfoColumn"
+          >编辑</span
+        >
+      </span>
+      <span v-else />
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+
+import { Row, Col } from 'element-ui'
+export default {
+  components: {
+    elRow: Row,
+    elCol: Col
+  },
+  props: {
+    previewIs: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data () {
+    return {
+      studentInfoList: []
+    }
+  },
+  computed: {
+
+    studentInfoArr () {
+      return this.titleInfo.filter((item) => item.checked === true)
+    }
+  },
+  watch: {
+    titleInfo (val) {
+      this.studentInfoList = JSON.parse(JSON.stringify(val))
+      window.console.log(val)
+    }
+  },
+  mounted () {
+    this.studentInfoList = JSON.parse(JSON.stringify(this.titleInfo))
+  },
+  methods: {
+    editStudentInfoColumn () {
+      this.$emit('hanldeStudent', this.studentInfoList)
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped></style>
