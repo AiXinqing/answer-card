4578a759b52eaf0cb1b4b1fbbf864ba47fc31764
qijiao <1003297709@qq.com>
2020-09-24 10:30:01 +0800

    修改学生信息样式及布局

--- src/views/page/subassembly/studentInfo.vue
+++ src/views/page/subassembly/studentInfo.vue
@@ -14,6 +14,7 @@
           >编辑</span
         >
       </span>
+      <span v-else />
     </el-col>
   </el-row>
 </template>
@@ -52,4 +53,38 @@ export default {
 }
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+  @import '~@/assets/css/publicColor.less';
+  .title-item {
+    height: 40px;
+    line-height: 50px;
+    min-width: 148px;
+    position: relative;
+    top: 8px;
+    span {
+      display: inline-block;
+      font-size: 18px;
+    }
+    > span:last-child {
+      width: 87px;
+      position: relative;
+      margin-left: 10px;
+      margin-right: 15px;
+      top: 3px;
+      border-bottom: 2px solid @font-888;
+    }
+    span.titke-edit {
+      position: absolute;
+      top: -26px;
+      right: 0;
+      font-size: 12px !important;
+      height: 20px;
+      line-height: 20px;
+      padding: 0 6px;
+      border-radius: 3px;
+      color: @white;
+      cursor: pointer;
+      background-color: @main;
+    }
+  }
+</style>
