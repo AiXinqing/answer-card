ff1e08949719399288679fbc7565a9fcbe1f1c4d
qijiao <1003297709@qq.com>
2020-09-25 17:08:31 +0800

    客观题模态框

---
+++ src/components/question-editor-modal.vue
@@ -0,0 +1,160 @@
+<template>
+  <hj-dialog
+    :title="title"
+    :visible.sync="dialogVisible"
+    :width="'600px'"
+    :before-close="cancel"
+  >
+    <div class="question-edit-wrap">
+      <!-- 题型参数 -->
+      <div class="layui-form">
+        <div class="layui-form-item">
+          <div class="label">大题题号:</div>
+          <hj-select
+            :items="options"
+            size="mini"
+            :value="1"
+            @change="hanldeSelect"
+          ></hj-select>
+        </div>
+        <div class="layui-form-item long">
+          <div class="label">题目:</div>
+          <el-input v-model="input" placeholder="请输入内容" />
+        </div>
+      </div>
+      <div class="layui-form">
+        <div class="layui-form-item">
+          <div class="label">每组题数:</div>
+          <el-input v-model="input" placeholder="请输入内容" />
+        </div>
+      </div>
+      <!-- 客观题tabs -->
+      <objective-question-tabs ref="objectiveTabs"/>
+      <!-- 插入操作 -->
+      <div class="insert-wrap">
+        <el-checkbox v-model="insert">插入添加题目</el-checkbox>
+        <div class="insert-select">
+          <span>插入到第</span>
+          <hj-select
+            :items="insertOptions"
+            size="mini"
+            :value="1"
+            @change="hanldeSelect"
+          ></hj-select>
+          <span>大题后</span>
+        </div>
+        <el-checkbox v-model="postpone">大题号自动顺延</el-checkbox>
+      </div>
+    </div>
+    <div class="dialog-footer ">
+      <hj-button type="cancel"  class="cancel"  @click="cancel">取 消</hj-button>
+      <hj-button type="cancel"  class="confirm"  @click="handleDetermine">确 定</hj-button>
+    </div>
+  </hj-dialog>
+</template>
+
+<script>
+import hjDialog from '@/components/elementUi/dialog'
+import hjSelect from '@/components/elementUi/select'
+import hjButton from '@/components/elementUi/button'
+
+import objectiveQuestionTabs from './modalSubassembly/objective-question-tabs'
+export default {
+  components: {
+    hjSelect,
+    hjDialog,
+    hjButton,
+    objectiveQuestionTabs
+  },
+  data () {
+    return {
+      dialogVisible: true,
+      input: 1,
+      options: [{
+        value: 1,
+        label: '一'
+      }],
+      insertOptions: [
+        {
+          value: 1,
+          label: '一.选择题'
+        }
+      ],
+      insert: true,
+      postpone: false
+    }
+  },
+  computed: {
+    title () {
+      return '新增客观题'
+    }
+  },
+  methods: {
+    cancel () {
+      this.close()
+    },
+    close () {
+      this.dialogVisible = false
+    },
+    hanldeSelect () {
+
+    },
+    handleDetermine () {
+
+    }
+  }
+}
+</script>
+
+<style lang="less" >
+  @import '~@/assets/css/publicColor.less';
+  .layui-form{
+    display: flex;
+    justify-content: start;
+    flex-wrap: wrap;
+
+    .layui-form-item{
+      width: 50%;
+      display: flex;
+      align-items:center;
+
+      .el-input__inner{
+        height: 30px;
+      }
+      .label{
+        width: 80px;
+      }
+
+      & >.el-input{
+        width: calc(100% - 130px)
+      }
+      .el-select{
+        width: calc(100% - 130px)
+      }
+      &.long{
+        .el-input{
+          width: calc(100% - 60px)
+        }
+      }
+    }
+
+    &:nth-child(n+2){
+      margin-top: 20px;
+    }
+  }
+  // 插入操作样式
+  .insert-wrap{
+    display:flex;
+    align-items:center;
+    margin-top:10px;
+
+    .insert-select{
+      width:260px;
+
+      .el-select{
+        margin:0 5px;
+        width: 130px;
+      }
+    }
+  }
+</style>
