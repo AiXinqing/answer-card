cdd2c44cfffad7d6e46bb42d84c2dbd19088b770
qijiao <1003297709@qq.com>
2020-09-25 16:23:56 +0800

    添加样式

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,29 @@
 </template>
 
 <style lang="less">
+  @import '~@/assets/css/publicColor.less';
   div#app {
     height: 100%;
     overflow: hidden;
+  }
+    // 公共样式
+  .el-select {
+    .el-input{
+      &.is-focus{
+        .el-input__inner{
+            color: @main;
+            border-color:@main;
+        }
+      }
+    }
+  }
+
+  .el-input__inner{
+      &:focus{
+        border-color: @main
+      }
+    }
+  .el-select-dropdown__item.selected{
+    color: @main
   }
 </style>
