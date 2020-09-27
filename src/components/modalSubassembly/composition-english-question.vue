fa0fc3e5e4aeddb52be786c3beecf6e8269aaba9
qijiao <1003297709@qq.com>
2020-09-27 17:31:45 +0800

    作文-英语模态框布局

---
+++ src/components/modalSubassembly/composition-english-question.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="question-content-warp">
+    <div class="layui-form">
+      <div class="layui-form-item">
+        <div class="label">小题题号:</div>
+        <el-input v-model="data.topic" placeholder="请输入小题题号" />
+        <div class="unit">题</div>
+      </div>
+    </div>
+    <div class="layui-form">
+      <div class="layui-form-item">
+        <div class="label">分数:</div>
+        <el-input v-model="data.score" placeholder="请输入分数" />
+        <div class="unit">分</div>
+      </div>
+    </div>
+    <div class="layui-form">
+      <div class="layui-form-item">
+        <div class="label">作文行数:</div>
+        <el-input v-model="data.row" placeholder="请输入作文行数" />
+        <div class="unit">行</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      data: {}
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+  .unit{
+    margin-left: 10px
+  }
+</style>
