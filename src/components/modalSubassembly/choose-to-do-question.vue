7bf63ea96c941cbef787315f7d1cf40f8499b5e1
qijiao <1003297709@qq.com>
2020-09-27 17:10:41 +0800

    选作题模态框布局

---
+++ src/components/modalSubassembly/choose-to-do-question.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="question-content-warp">
+    <!-- 题组 -->
+    <choose-grouping/>
+    <!-- 分段渲染 -->
+    <choose-group-detail/>
+    <div class="additional-conditions">
+      <el-checkbox v-model="ShowScore">小题显示分数</el-checkbox>
+    </div>
+  </div>
+</template>
+
+<script>
+import chooseGrouping from './choose/choose-grouping'
+import chooseGroupDetail from './choose/choose-groups-detail'
+export default {
+  components: {
+    chooseGrouping,
+    chooseGroupDetail
+  },
+  data () {
+    return {
+      ShowScore: false
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+  .additional-conditions{
+    display: flex;
+    margin: 10px 0;
+  }
+</style>
---
+++ src/components/modalSubassembly/choose/choose-grouping.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="question-group-wrap">
+    <div class="question-group-item choose">
+      <span>几选几:</span>
+      <el-input v-model.number="data.choices" size="mini"   onkeyup="this.value = this.value.replace(/[^\d.]/g,'');" />
+      <span> 选 </span>
+      <el-input v-model.number="data.select" size="mini"  onkeyup="this.value = this.value.replace(/[^\d.]/g,'');" />
+      <span> ;每题 </span>
+      <el-input v-model.number="data.score" size="mini"  onkeyup="this.value = this.value.replace(/[^\d.]/g,'');" />
+      <span> 分;题号 </span>
+      <el-input v-model.number="data.start" size="mini"  onkeyup="this.value = this.value.replace(/[^\d.]/g,'');" />
+      <span> 到</span>
+      <el-input v-model.number="data.end" size="mini" disabled  onkeyup="this.value = this.value.replace(/[^\d.]/g,'');" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      data: {}
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+  .question-group-wrap{
+    margin-top: 20px;
+    .question-group-item{
+      &.choose{
+
+        .el-input{
+          &.el-input--mini{
+            width: 68px;
+          }
+        }
+      }
+    }
+  }
+</style>
