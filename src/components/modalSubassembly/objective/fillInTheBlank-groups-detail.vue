e5fde5be2401cb916f54d5e1dd72ae69d55c034c
qijiao <1003297709@qq.com>
2020-09-27 15:38:04 +0800

    填空题渲染部分

---
+++ src/components/modalSubassembly/objective/fillInTheBlank-groups-detail.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="question-groups-detail">
+    <div class="grouping-detail-info">
+      <div class="question-number">1</div>
+      <div class="question-info">
+        <el-input v-model.number="data.score" size="mini"  onkeyup="this.value = this.value.replace(/[^\d.]/g,'');"/>
+        <span>分</span>
+        <el-input v-model.number="data.select" size="mini"  onkeyup="this.value = this.value.replace(/[^\d.]/g,'');"/>
+        <span>个选项</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      data: {}
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/modalSubassembly/objective/fillInTheBlank-groups.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="question-group-wrap">
+    <div class="question-group-item">
+      <span>从</span>
+      <el-input v-model.number="data.start" size="mini"    onkeyup="this.value = this.value.replace(/[^\d.]/g,'');" />
+      <span>题到</span>
+      <el-input v-model.number="data.end" size="mini"   onkeyup="this.value = this.value.replace(/[^\d.]/g,'');" />
+      <span>题,每空</span>
+      <el-input v-model.number="data.score" size="mini"   onkeyup="this.value = this.value.replace(/[^\d.]/g,'');" />
+      <span>分,每题</span>
+      <el-input v-model.number="data.space" size="mini"   onkeyup="this.value = this.value.replace(/[^\d.]/g,'');"/>
+      <span>空</span>
+      <i class="el-icon-delete" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      data: { }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
