68df9aa4ffdb937abe08957c5ec7d4611e5d344c
qijiao <1003297709@qq.com>
2020-09-27 15:35:59 +0800

    填空题模态框组件

---
+++ src/components/modalSubassembly/fill-in-the-blank-modal.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="question-content-warp">
+    <div class="layui-form">
+      <div class="layui-form-item">
+        <div class="label">每组题数:</div>
+        <el-input v-model="data.groupSize" placeholder="请输入每组题数" />
+      </div>
+    </div>
+
+    <!-- 填空题题组 -->
+    <fill-in-the-blank-groups/>
+
+    <!-- 分段小题 -->
+    <div class="add-question-group">+ 分段添加小题</div>
+
+    <!-- 小题详情 -->
+
+  </div>
+</template>
+
+<script>
+import fillInTheBlankGroups from './objective/fillInTheBlank-groups'
+export default {
+  components: {
+    fillInTheBlankGroups
+  },
+  data () {
+    return {
+      data: {}
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
