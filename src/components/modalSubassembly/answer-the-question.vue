98a7b78f0a8935c2e8035fac35e270aff40f9478
qijiao <1003297709@qq.com>
2020-09-27 16:54:49 +0800

    解答题模态框组件布局

---
+++ src/components/modalSubassembly/answer-the-question.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="question-content-warp">
+    <!-- 题组 -->
+    <answer-groups/>
+    <!-- 分段渲染 -->
+    <answer-groups-detail/>
+    <div class="additional-conditions">
+      <el-checkbox v-model="ShowScore">小题显示分数</el-checkbox>
+      <el-checkbox v-model="HorizontalLine">生成解答题横线</el-checkbox>
+    </div>
+  </div>
+</template>
+
+<script>
+import answerGroups from './answer/answer-groups'
+import answerGroupsDetail from './answer/answer-groups-detail'
+export default {
+  components: {
+    answerGroups,
+    answerGroupsDetail
+  },
+  data () {
+    return {
+      ShowScore: false,
+      HorizontalLine: false
+    }
+  }
+}
+</script>
+
+<style lang="less">
+  .additional-conditions{
+    display: flex;
+    margin: 10px 0;
+  }
+</style>
---
+++ src/components/modalSubassembly/answer/answer-groups.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="question-group-wrap">
+    <div class="question-group-item answer">
+      <span>从</span>
+      <el-input v-model.number="data.start" size="mini"   onkeyup="this.value = this.value.replace(/[^\d.]/g,'');" />
+      <span class="p-5"> 题到 </span>
+      <el-input v-model.number="data.end" size="mini"  onkeyup="this.value = this.value.replace(/[^\d.]/g,'');" />
+      <span class="p-5"> 题 </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      data: {}
+    }
+  }
+}
+</script>
+
+<style lang="less">
+.question-group-wrap{
+  .question-group-item{
+    &.answer{
+      .el-input{
+        &.el-input--mini{
+          width: 68px;
+        }
+      }
+    }
+  }
+}
+</style>
