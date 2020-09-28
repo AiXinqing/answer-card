95fbebea3e123a15f9dac00e473ae6c542997de5
qijiao <1003297709@qq.com>
2020-09-28 16:10:35 +0800

    删除无效验证

--- src/components/modalSubassembly/objective/grouping-question.vue
+++ src/components/modalSubassembly/objective/grouping-question.vue
@@ -1,33 +1,29 @@
 <template>
-  <div class="question-groups-detail">
-    <div
-      class="grouping-detail-info"
-      v-for="question in data"
-      :key="question.uuid"
-    >
-      <div class="question-number">1</div>
-      <div class="question-info">
-        <!-- 多选 -->
-        <template v-if="questionType =='multipleChoice'">
-          <el-input v-model.number="question.halfScore" size="mini"  onkeyup="this.value = this.value.replace(/[^\d.]/g,'');"/>
-          <span>分,少选得</span>
-          <el-input v-model.number="question.score" size="mini"  :max="data.score" onkeyup="this.value = this.value.replace(/[^\d.]/g,'');" />
-          <span>分</span>
-        </template>
+  <div
+    class="grouping-detail-info"
+  >
+    <div class="question-number">{{data.serialNumber}}</div>
+    <div class="question-info">
+      <!-- 多选 -->
+      <template v-if="questionType =='multipleChoice'">
 
-        <template v-else>
-          <el-input v-model.number="question.score" size="mini"  onkeyup="this.value = this.value.replace(/[^\d.]/g,'');"/>
-          <span>分</span>
-        </template>
+        <el-input v-model.number="data.halfScore" size="mini" />
+        <span>分,少选得</span>
+        <el-input v-model.number="data.score" size="mini"  :max="data.score" />
+        <span>分</span>
+      </template>
 
-        <el-input
-          v-model.number="question.optionLength"
-          size="mini"
-          :disabled="questionType == 's' ? true:false"
-          onkeyup="this.value = this.value.replace(/[^\d.]/g,'');"
-        />
-        <span>个选项</span>
-      </div>
+      <template v-else>
+        <el-input v-model.number="data.score" size="mini" />
+        <span>分</span>
+      </template>
+
+      <el-input
+        v-model.number="data.optionLength"
+        size="mini"
+        :disabled="questionType == 's' ? true:false"
+      />
+      <span>个选项</span>
     </div>
   </div>
 </template>
@@ -36,8 +32,8 @@
 export default {
   props: {
     groupsQuestion: {
-      type: Array,
-      default: () => []
+      type: Object,
+      default: () => {}
     },
     questionType: {
       type: String,
