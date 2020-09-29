e153bf94995a8e6626ae06d020f5ed2179b05e7d
qijiao <1003297709@qq.com>
2020-09-29 11:50:42 +0800

    客观题-单选-题列表

--- src/components/QuestionEditorModel/objective/singleChoice/question-list.vue
+++ src/components/QuestionEditorModel/objective/singleChoice/question-list.vue
@@ -1,15 +1,52 @@
 <template>
-  <div>
-
+  <div
+    class="grouping-detail-info"
+  >
+    <div class="question-number">{{data.serialNumber}}</div>
+    <div class="question-info">
+      <el-input v-model.number="data.score" @blur="updateSubquestion" size="mini" />
+      <span>分</span>
+      <el-input
+        v-model.number="data.optionLength"
+        size="mini"
+        @blur="updateSubquestion"
+      />
+      <span>个选项</span>
+    </div>
   </div>
 </template>
 
 <script>
+import ObjectiveQuestion from '@/models/question/objective'
 export default {
-
+  props: {
+    groupsQuestion: {
+      type: Object,
+      default: () => {}
+    },
+    question: {
+      type: ObjectiveQuestion,
+      required: true
+    }
+  },
+  data () {
+    return {
+      data: {}
+    }
+  },
+  watch: {
+    groupsQuestion: {
+      immediate: true,
+      handler (question) {
+        this.data = this.groupsQuestion
+      }
+    }
+  },
+  methods: {
+    updateSubquestion () {
+      const { subquestions } = this.question
+      subquestions.singleChoice.updateSubquestion(this.data)
+    }
+  }
 }
 </script>
-
-<style lang="scss" scoped>
-
-</style>
