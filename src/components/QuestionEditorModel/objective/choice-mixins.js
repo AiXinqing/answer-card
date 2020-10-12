37e0013f3664073fb6c665451636febc66910130
moonRider <solar.pom@gmail.com>
2020-10-12 21:21:07 +0800

    feat: use choice group mixins
    修复一些逻辑错误

--- src/components/QuestionEditorModel/objective/choice-mixins.js
+++ src/components/QuestionEditorModel/objective/choice-mixins.js
@@ -85,5 +85,74 @@ export const IndexMixins = {
 }
 
 export const GroupMixins = {
+  props: {
+    group: {
+      type: Object,
+      required: true
+    },
+    question: {
+      type: Object,
+      required: true
+    }
+  },
 
+  inject: ['sheet'],
+
+  data () {
+    return {
+      data: null,
+      errorMessage: ''
+    }
+  },
+
+  computed: {
+    errorPrompt () {
+      const { endNumber, score, startNumber } = this.data
+      return endNumber <= 0 ? '结束题号必须大于0'
+        : endNumber && startNumber === 0 ? '开始题号必须大于0'
+          : endNumber && endNumber < startNumber ? '开始题号不能大于结束题号'
+            : endNumber && !score ? '分数不能为空' : ''
+    }
+  },
+
+  watch: {
+    group: {
+      immediate: true,
+      handler () {
+        this.data = {
+          ...this.group
+        }
+      }
+    }
+  },
+
+  methods: {
+    checkGroupValid () {
+      this.errorMessage = this.errorPrompt
+      if (this.errorMessage) return false
+
+      const { startNumber, endNumber } = this.data
+      const serialNumbers = Array.from({ length: endNumber - startNumber + 1 })
+        .map((_, index) => index + startNumber)
+      return serialNumbers.every(number => {
+        if (this.group.uuid && (this.group.startNumber <= number && this.group.endNumber >= number)) return true
+        const valid = this.sheet.isSubquestionSerialNumberVaild(number) &&
+          !this.question.serialNumberSet.has(number)
+        if (!valid) {
+          this.errorMessage = `第${number}题已经存在，请勿重复添加`
+        }
+        return valid
+      })
+    },
+
+    fireGroupChange () {
+      this.checkGroupValid()
+
+      if (this.errorMessage) {
+        this.$emit('check-fail', this.errorMessage)
+      } else {
+        this.$emit('group-valid', { ...this.data })
+      }
+    }
+  }
 }
